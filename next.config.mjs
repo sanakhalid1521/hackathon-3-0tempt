fada8be3e748f44deaf793341a50e0bae6a75bb6
Sana Khalid <160645960+sanakhalid1521@users.noreply.github.com>
2025-01-23 20:56:08 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ["cdn.sanity.io"], // Make sure to use "domains" instead of "domain"
     },
+    eslint: {
+        ignoreDuringBuilds: true, 
+    },
 };
 
 export default nextConfig;
